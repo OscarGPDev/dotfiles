e3f874375edb6af64706eda885adfe4b0588aad3
OscarGPDev <oscarprograb@gmail.com>
2022-02-10 22:32:55 -0600

    script de instalación provisional

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#cp -r ../!(starshiprs|utils|README.md|test) ../test/
+cp -r alacritty $HOME/.config
+cp -r dunst $HOME/.config
+cp -r i3 $HOME/.config
+cp -r kitty $HOME/.config
+cp -r picom $HOME/.config
+cp starshiprs/starship.toml $HOME/.config
+cp -r utils $HOME
