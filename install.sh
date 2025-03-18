edd7c5652641a87b54430a114d1dd98970f2307e
Oscar GP <oscarprograb@gmail.com>
2025-03-18 08:52:39 -0600

    Se agrega mi configuracion de Ghostty :)

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #cp -r ../!(starshiprs|utils|README.md|test) ../test/
 cp -r alacritty $HOME/.config
+cp -r ghostty $HOME/.config
 cp -r dunst $HOME/.config
 cp -r i3 $HOME/.config
 cp -r kitty $HOME/.config
